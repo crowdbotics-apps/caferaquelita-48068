ebee1cb53842fc0f8126f7573714da545af2c7e1
Crowdbotics <team@crowdbotics.com>
2024-05-22 21:31:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "caferaquelita-48068",
+  url: "https://caferaquelita-48068.botics.co",
+  api: "https://caferaquelita-48068.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
